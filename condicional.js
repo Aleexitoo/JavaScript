8bb5074d9ac98c4851e077cb891f0452fe0c2df5
Alejandro <ilextron@gmail.com>
2021-10-21 12:00:54 +0200

    if else

--- condicional.js
+++ condicional.js
@@ -1,8 +1,13 @@
-var a = 12;
+var a = 20;
 var b = 20;
 if (a > b) {
     console.log(`${a} es mayor a: ${b}`);
 }
+else if(a == b){
+console.log("a y b son iguales")
+}
+
+
 else{
     console.log(`${a} es menor a: ${b}`);
 }
