428ba6542c14de33def977fe0a57d5b5769bc8ed
Alejandro <ilextron@gmail.com>
2021-10-21 12:03:22 +0200

    simobolo &&

--- condicional.js
+++ condicional.js
@@ -1,13 +1,3 @@
-var a = 20;
-var b = 20;
-if (a > b) {
-    console.log(`${a} es mayor a: ${b}`);
-}
-else if(a == b){
-console.log("a y b son iguales")
-}
-
-
-else{
-    console.log(`${a} es menor a: ${b}`);
-}
+var x = true;
+var y = false;
+console.log( x && y);
