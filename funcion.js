7022c20474d3d0ac8aaddad37c206823fd8c8669
Alejandro <ilextron@gmail.com>
2021-10-21 15:26:16 +0200

    funcion anidada

--- funcion.js
+++ funcion.js
@@ -1,2 +1,13 @@
-var nombre = () => "victor";
-console.log(nombre());
+function operacion() {
+    const PI = 3.14;
+    function area(radio) {
+        var area = PI * radio * radio;
+        console.log("El area del circulo es: " +area);
+
+    }
+    operacion.area = area;
+}
+
+var radio = 4;
+operacion();
+operacion.area(radio);
